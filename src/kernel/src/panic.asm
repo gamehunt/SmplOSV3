99a178547630e0b4486e5ae6275011e8bb921934
gamehunt <mt.demakov@gmail.com>
2021-07-01 20:33:05 +0300

    additional registers forpanic

---
+++ src/kernel/src/panic.asm
@@ -0,0 +1,9 @@
+global get_cr2
+get_cr2:
+    mov rax, cr2
+    ret
+
+global get_cr3
+get_cr3:
+    mov rax, cr3
+    ret
--- src/kernel/src/panic.c
+++ src/kernel/src/panic.c
@@ -1,6 +1,11 @@
 #include <panic.h>
 #include <dev/log.h>
 
+extern uint64_t get_cr1();
+extern uint64_t get_cr2();
+extern uint64_t get_cr3();
+extern uint64_t get_cr4();
+
 void panic(regs_t dump, const char* message){
     asm("cli");
     error("--------------------------------------------------------------------");
@@ -14,6 +19,7 @@ void panic(regs_t dump, const char* message){
         error("\tR8:  0x%llx | R9:  0x%llx | R10: 0x%llx | R11: 0x%llx", dump->r8, dump->r9, dump->r10, dump->r11);
         error("\tR12: 0x%llx | R13: 0x%llx | R14: 0x%llx | R15: 0x%llx", dump->r12, dump->r13, dump->r14, dump->r15);
         error("\tRIP: 0x%llx | RFLAGS: 0x%llx", dump->rip, dump->rflags);
+        error("\tCR2: 0x%llx | CR3: 0x%llx", get_cr2(), get_cr3());
     }else{
         error("\t<Not available>");
     }
