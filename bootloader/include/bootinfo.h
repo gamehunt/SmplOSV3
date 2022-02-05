30271b4c58e6b5202dbfeab086157fef75ad93a1
mt.demakov@gmail.com <mt.demakov@gmail.com>
2022-02-05 15:15:45 +0300

    getting memmap and fb info

--- bootloader/include/bootinfo.h
+++ bootloader/include/bootinfo.h
@@ -7,8 +7,29 @@
     #include <boot/commons.h>
 #endif
 
+#define B_MEMTYPE_RESERVED 0
+#define B_MEMTYPE_ALLOWED  1
+#define B_MEMTYPE_ACPI     2
+
 typedef struct{
-    uint8_t dummy;
+    uint64_t start;
+    uint64_t size; //In pages
+    uint8_t type;   
+}memmap_entry_t;
+
+typedef struct{
+    memmap_entry_t* entries;
+     uint32_t size;
+}memmap_info_t;
+
+typedef struct{
+    uint64_t addr;
+    uint64_t size;
+}fb_info_t;
+
+typedef struct{
+    memmap_info_t* memmap;
+    fb_info_t* framebuffer;
 }bootinfo_t;
 
 #endif
--- bootloader/src/main.c
+++ bootloader/src/main.c
@@ -6,6 +6,16 @@
 #include <bootinfo.h>
 #include <mem.h>
 
+static uint8_t get_memory_type(uint8_t type){
+    if(type == 1 || type == 2 || type == 3 || type == 4 || type == 7){
+        return B_MEMTYPE_ALLOWED;
+    }else if(type == 9){
+        return B_MEMTYPE_ACPI;
+    }else{
+        return B_MEMTYPE_RESERVED;
+    }
+}
+
 int main(int argc, char **argv)
 {
     UNUSED(argc);
@@ -14,7 +24,96 @@ int main(int argc, char **argv)
     ST->ConOut->ClearScreen(ST->ConOut);
     ST->BootServices->SetWatchdogTimer(0, 0, 0, NULL);
 
-    b_info("SmplOSV3.1 Loader started");
+    bootinfo_t bi;
+
+    b_info("-------------SmplOSV3.1 Loader-------------");
+
+    b_info("Retrieving memory map...");
+
+    efi_status_t status;
+    efi_memory_descriptor_t *memory_map = NULL, *mement;
+    uintn_t memory_map_size=0, map_key=0, desc_size=0;
+
+    status = BS->GetMemoryMap(&memory_map_size, NULL, &map_key, &desc_size, NULL);
+    if(status != EFI_BUFFER_TOO_SMALL || !memory_map_size){
+        b_error("Failed to get memory map.");
+        return 0;
+    }
+
+    memory_map_size += 4 * desc_size;
+    memory_map = (efi_memory_descriptor_t*)malloc(memory_map_size);
+    if(!memory_map) {
+        b_error("Malloc failure");
+        return 0;
+    }
+
+    status = BS->GetMemoryMap(&memory_map_size, memory_map, &map_key, &desc_size, NULL);
+    if(EFI_ERROR(status)) {
+        b_error("Failed to get memory map.");
+        return 0;
+    }
+
+    bi.memmap = malloc(sizeof(memmap_info_t));
+
+    bi.memmap->size = 0;
+    bi.memmap->entries = NULL;
+
+    for(mement = memory_map; (uint8_t*)mement < (uint8_t*)memory_map + memory_map_size;
+        mement = NextMemoryDescriptor(mement, desc_size)) {
+        if(bi.memmap->entries && bi.memmap->entries[bi.memmap->size - 1].type == get_memory_type(mement->Type) && ((bi.memmap->entries[bi.memmap->size - 1].start + bi.memmap->entries[bi.memmap->size - 1].size * 0x1000) == mement->PhysicalStart)){
+            bi.memmap->entries[bi.memmap->size - 1].size += mement->NumberOfPages;
+            continue;
+        }
+        
+        bi.memmap->size++;
+        if(!bi.memmap->entries){
+            bi.memmap->entries = malloc(sizeof(memmap_entry_t));
+        }else{
+            bi.memmap->entries = realloc(bi.memmap->entries, bi.memmap->size*sizeof(memmap_entry_t));
+        }
+        bi.memmap->entries[bi.memmap->size - 1].start = mement->PhysicalStart;
+        bi.memmap->entries[bi.memmap->size - 1].size = mement->NumberOfPages;
+        bi.memmap->entries[bi.memmap->size - 1].type = get_memory_type(mement->Type);
+    }
+
+    free(memory_map);
+
+    for (uint32_t i = 0; i < bi.memmap->size-1; i++){
+       for (uint32_t j = 0; j < bi.memmap->size-i-1; j++){
+           if (bi.memmap->entries[j].start > bi.memmap->entries[j+1].start){
+              memmap_entry_t t = bi.memmap->entries[j];
+              bi.memmap->entries[j] = bi.memmap->entries[j + 1];
+              bi.memmap->entries[j + 1] = t;
+           }
+       }
+    }
+
+    for (uint32_t i = 0; i < bi.memmap->size; i++){
+        if(bi.memmap->entries[i].type == B_MEMTYPE_ALLOWED){
+            b_info("Usable memory range found: 0x%x - 0x%x", bi.memmap->entries[i].start, bi.memmap->entries[i].start + bi.memmap->entries[i].size * 0x1000);
+        }
+    }
+
+    b_info("Getting framebuffer info...");
+
+    bi.framebuffer = malloc(sizeof(fb_info_t));
+
+    efi_guid_t gopGuid = EFI_GRAPHICS_OUTPUT_PROTOCOL_GUID;
+    efi_gop_t *gop = NULL;
+    efi_gop_mode_info_t *info = NULL;
+    UNUSED(info);
+
+    status = BS->LocateProtocol(&gopGuid, NULL, (void**)&gop);
+
+    if(!EFI_ERROR(status) && gop) {
+        bi.framebuffer->addr = gop->Mode->FrameBufferBase;
+        bi.framebuffer->size = gop->Mode->FrameBufferSize;
+        b_info("Found framebuffer: 0x%x - 0x%x", bi.framebuffer->addr, bi.framebuffer->addr + bi.framebuffer->size);
+    }else{
+        b_error("Unable to get graphics output protocol.");
+        return 0;
+    }
+
     b_info("Trying to parse boot.cfg...");
 
     config_result_t  data;
@@ -47,7 +146,15 @@ int main(int argc, char **argv)
               sprintf(&kernel_path[0], "\\smplos\\%s", data.childs[i].value);
           } 
           else if(!strcmp(data.childs[i].name, "video")){
-              b_info("Found graphics mode entry: %s", data.childs[i].value);
+            b_info("Found graphics mode entry: %s", data.childs[i].value);
+            status = gop->SetMode(gop, atoi(data.childs[i].value));
+            ST->ConOut->Reset(ST->ConOut, 0);
+            ST->StdErr->Reset(ST->StdErr, 0);
+            if(EFI_ERROR(status)) {
+                b_error("Unable to set video mode\n");
+            }else{
+                b_info("Graphics mode set to %s", data.childs[i].value);
+            }
           }   
           free(data.childs[i].name);
           free(data.childs[i].value);
@@ -83,8 +190,6 @@ int main(int argc, char **argv)
         return 0;
     }
 
-    b_setup_paging();
-
     uintptr_t entry;
     BSTATUS s;
     if((s = load_elf(buff, &entry))){
@@ -104,7 +209,6 @@ int main(int argc, char **argv)
         b_info("Kernel entry point: 0x%x", entry);
     }
 
-    bootinfo_t bi;
     if(exit_bs()){
         b_error("Failed to exit boot services!");
     }else{
